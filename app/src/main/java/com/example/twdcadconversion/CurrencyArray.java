ccb8972d5c4b1f596e14933702f43f9d17fa2a8a
carter <carterruscica@hotmail.com>
2019-06-14 20:36:55 -0400

    Update v1.01 - Included the ability to search for more than one currency
    Upcoming
    - Save program from where it last stood (could be for all variables)
    - Save previous rates in .json file
    - Include flags that go along with each supported currency
    - Suggest which currencies are supported

---
+++ app/src/main/java/com/example/twdcadconversion/CurrencyArray.java
@@ -0,0 +1,121 @@
+package com.example.twdcadconversion;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CurrencyArray {
+    private static final String CURL = "https://www.bankofcanada.ca/valet/observations/FX";
+    private static final String INFO_URL = ("https://www.bankofcanada.ca/valet/observations/group/FX_RATES_DAILY/csv?recent=1");
+    private static final String FILENAME = "data.txt";
+    private static final String DOWNLOAD_FILE = "currency.csv";
+    List<rateObject> rateList = new ArrayList<>();
+    private int length;
+
+    public CurrencyArray(){
+        try{
+            downloadData();
+        }catch(IOException e){
+            System.out.println("File could not be downloaded");
+        }
+        csvToObject();
+    }
+
+    public Double getRate(String c1, String c2){
+        rateObject temp;
+        Double frate = 1.0, srate = 1.0;
+        for (int i = 0; i < length-1; i++){
+            temp = rateList.get(i);
+            if (temp.c1.equals(c1)){
+                System.out.println("We have the 1st object: " + temp.toString());
+                frate = temp.rate;
+            }
+            if (temp.c1.equals(c2)){
+                System.out.println("We have the 2nd object: " + temp.toString());
+                srate = temp.rate;
+            }
+        }
+        return frate/srate;
+    }
+
+
+    /**
+     * This method uses the downloaded file of CSV values then to load it and save it onto the device.
+     *
+     */
+    private void csvToObject(){
+        String line;
+        try(Scanner input = new Scanner(new File(DOWNLOAD_FILE))){
+            while(input.hasNext()) {
+                line = input.nextLine();
+                if (line.equals("OBSERVATIONS")){
+                    String[] csvCList = input.nextLine().split(",");
+                    String[] csvRateList = input.nextLine().split(",");
+                    String date = csvRateList[0];
+                    rateObject temp;
+                    length = csvRateList.length;
+                    for (int i = 1; i < length; i++){
+                        temp = new rateObject(Double.parseDouble(csvRateList[i]), csvCList[i].substring(2, 5), csvCList[i].substring(5, 8), date);
+                        rateList.add(temp);
+                    }
+                }
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method downloads and saves the data from the INFO_URL
+     * @throws IOException
+     */
+    private void downloadData() throws IOException {
+        URL InfoURL = new URL(INFO_URL);
+        ReadableByteChannel rbc = Channels.newChannel(InfoURL.openStream());
+        FileOutputStream fos = new FileOutputStream(DOWNLOAD_FILE);
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        fos.close();
+    }
+
+    /**
+     * This method is for debugging
+     */
+    public void printList(){
+        int size = rateList.size(), index = 0;
+        rateObject temp;
+        while(index < size){
+            temp = rateList.get(index);
+            index++;
+            System.out.println("Our Objects: " + temp.toString());
+        }
+    }
+
+
+
+}
+
+class rateObject{
+    double rate;
+    String date;
+    String c1, c2;
+
+    public rateObject(double rate, String c1, String c2, String date){
+        this.rate = rate;
+        this.c1 = c1;
+        this.c2 = c2;
+        this.date = date;
+    }
+
+    @Override
+    public String toString(){
+        return (this.c1 + this.c2 + " " + this.rate + " " + this.date);
+    }
+
+}
--- app/src/main/java/com/example/twdcadconversion/MainActivity.java
+++ app/src/main/java/com/example/twdcadconversion/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.twdcadconversion;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -19,89 +23,23 @@ public class MainActivity extends AppCompatActivity{
     TextView output, currency0, currency1, currType0, currType1;
     Context context;
     double rate;
-    String currTi0 = "", currTi1 = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        context = getApplicationContext();
-
-        Button button = (Button)findViewById(R.id.button2);
-        toggle = (Switch)findViewById(R.id.switch1);
-        cur0 = toggle.isChecked();
-        output = (TextView)findViewById(R.id.textView3);
-
-        currency0 = (TextView)findViewById(R.id.editText3); // TWD
-        currType0 = (TextView)findViewById(R.id.editText2); // TWD
-        currTi0 = currType0.getText().toString();
-
-        currency1 = (TextView)findViewById(R.id.editText4); // CAD
-        currType1 = (TextView)findViewById(R.id.editText); // CAD
-        currTi1 = currType1.getText().toString();
-
-        toggle.setText("Convert " + currTi0);
-
-        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                toggle.setText("Convert " + currTi0);
-            }
-        });
-
-
-
-
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dataGetter dg = new dataGetter();
-                FileHelper fh = new FileHelper();
-
-                currTi0 = currType0.getText().toString();
-                toggle.setText("Convert " + currTi0);
-                currTi1 = currType1.getText().toString();
-
-                String input = fh.readFromFile(context);
-                try{
-                    if(dg.isOnline(context))
-                        input = dg.getData(context, currTi0, currTi1);
-                }catch(IOException e){
-//                    input = fh.readFromFile(context);
-                }
-                rate = Double.parseDouble(input);
-                convert();
-            }
-        });
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
+                (this, R.array.currencies, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
 
     }
 
     private void convert(){
 
-
-        double cash;
-        if (toggle.isChecked()){
-            if (currency1.getText().toString().length() >0)
-                cash = new Double(currency1.getText().toString()) * rate;
-            else
-                cash = 0;// convert to Taiwan dollars
-
-            output.setText(currency1.getText().toString() + " " + currTi0 +
-                    "\nis\n" + String.format("%.2f", cash) + " " + currTi1);
-            currency0.setText(Double.toString(round(cash,2)));
-
-        }else{
-            if (currency0.getText().toString().length() >0)
-                cash = new Double(currency0.getText().toString()) / rate;
-            else
-                cash = 0;
-            output.setText(currency0.getText() + " " + currTi1 +
-                    "\nis\n" + String.format("%.2f", cash) + " " + currTi0);
-            currency1.setText(Double.toString(round(cash, 2)));
-        }
     }
 
     private static double round(double value, int place) {
@@ -110,6 +48,15 @@ public class MainActivity extends AppCompatActivity{
         value = value * factor;
         long tmp = Math.round(value);
         return (double) tmp / factor;
+    }
+
+    private boolean isOnline(Context c){
+        ConnectivityManager connMgr = (ConnectivityManager) c.getSystemService(c.CONNECTIVITY_SERVICE);
+        NetworkInfo netWorkInfo = connMgr.getActiveNetworkInfo();
+        if (netWorkInfo != null && netWorkInfo.isConnected())
+            return true;
+        else
+            return false;
     }
 
 }
